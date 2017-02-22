ca0f5b4a1f9a01845e43d6ce784ee5f4e3f29060
Phil Smith <philsmith@Phil-Smith.local>
2017-02-22 14:28:10 +1000

    Add User and Micropost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "My future toy app"
+    render html: "My future toy app - change 1"
   end
 end
